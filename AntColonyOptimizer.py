8dcf98b286856b844c7e11c3da506b6bb9cc884d
John Berroa <jberroa@agile-im.de>
2018-10-10 19:18:32 +0200

    First push

---
+++ AntColonyOptimizer.py
@@ -0,0 +1,259 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+class AntColonyOptimizer:
+    def __init__(self, ants, evaporation_rate, intensification, alpha=1, beta=0, beta_evaporation_rate=0,
+                 choose_best=.1):
+        """
+        Ant colony optimizer.  Traverses a graph and finds either the max or min distance between nodes.
+        :param ants: number of ants to traverse the graph
+        :param evaporation_rate: rate at which pheromone evaporates
+        :param intensification: constant added to the best path
+        :param alpha: weighting of pheromone
+        :param beta: weighting of heuristic (1/distance)
+        :param beta_evaporation_rate: rate at which beta decays (optional)
+        :param choose_best: probability to choose the best route
+        """
+        # Parameters
+        self.ants = ants
+        self.evaporation_rate = evaporation_rate
+        self.pheromone_intensification = intensification
+        self.heuristic_alpha = alpha
+        self.heuristic_beta = beta
+        self.beta_evaporation_rate = beta_evaporation_rate
+        self.choose_best = choose_best
+
+        # Internal representations
+        self.pheromone_matrix = None
+        self.heuristic_matrix = None
+        self.probability_matrix = None
+
+        self.map = None
+        self.set_of_available_nodes = None
+
+        # Internal stats
+        self.best_series = []
+        self.best = None
+        self.fitted = False
+        self.best_path = None
+        self.fit_time = None
+
+        # Plotting values
+        self.plot_y_coord = None
+        self.plot_x_coord = None
+
+    def __str__(self):
+        string = "Ant Colony Optimizer"
+        string += "\n--------------------"
+        string += "\nDesigned to optimize either the minimum or maximum distance between nodes in a square matrix that behaves like a distance matrix."
+        string += "\n--------------------"
+        string += "\nNumber of ants:\t\t\t\t{}".format(self.ants)
+        string += "\nEvaporation rate:\t\t\t{}".format(self.evaporation_rate)
+        string += "\nIntensification factor:\t\t{}".format(self.pheromone_intensification)
+        string += "\nAlpha Heuristic:\t\t\t{}".format(self.heuristic_alpha)
+        string += "\nBeta Heuristic:\t\t\t\t{}".format(self.heuristic_beta)
+        string += "\nBeta Evaporation Rate:\t\t{}".format(self.beta_evaporation_rate)
+        string += "\nChoose Best Percentage:\t\t{}".format(self.choose_best)
+        string += "\n--------------------"
+        string += "\nUSAGE:"
+        string += "\nNumber of ants influences how many paths are explored each iteration."
+        string += "\nThe alpha and beta heuristics affect how much influence the pheromones or the distance heuristic weigh an ants' decisions."
+        string += "\nBeta evaporation reduces the influence of the heuristic over time."
+        string += "\nChoose best is a percentage of how often an ant will choose the best route over probabilistically choosing a route based on pheromones."
+        string += "\n--------------------"
+        if self.fitted:
+            string += "\n\nThis optimizer has been fitted."
+        else:
+            string += "\n\nThis optimizer has NOT been fitted."
+        return string
+
+    def _initialize(self):
+        """
+        Initializes the model by creating the various matrices and generating the list of available nodes
+        """
+        assert self.map.shape[0] == self.map.shape[1], "Map is not a distance matrix!"
+        num_nodes = self.map.shape[0]
+        self.pheromone_matrix = np.ones((num_nodes, num_nodes))
+        # Remove the diagonal since there is no pheromone from node i to itself
+        self.pheromone_matrix[np.eye(num_nodes) == 1] = 0
+        self.heuristic_matrix = 1 / self.map
+        self.probability_matrix = (self.pheromone_matrix ** self.heuristic_alpha) * (
+                self.heuristic_matrix ** self.heuristic_beta)  # element by element multiplcation
+        self.set_of_available_nodes = list(range(num_nodes))
+
+    def _reinstate_nodes(self):
+        """
+        Resets available nodes to all nodes for the next iteration
+        """
+        self.set_of_available_nodes = list(range(self.map.shape[0]))
+
+    def _update_probabilities(self):
+        """
+        After evaporation and intensification, the probability matrix needs to be updated.  This function
+        does that.
+        """
+        self.probability_matrix = (self.pheromone_matrix ** self.heuristic_alpha) * (
+                self.heuristic_matrix ** self.heuristic_beta)
+
+    def _choose_next_node(self, from_node):
+        """
+        Chooses the next node based on probabilities.  If p < p_choose_best, then the best path is chosen, otherwise
+        it is selected from a probability distribution weighted by the pheromone.
+        :param from_node: the node the ant is coming from
+        :return: index of the node the ant is going to
+        """
+        numerator = self.probability_matrix[from_node, self.set_of_available_nodes]
+        if np.random.random() < self.choose_best:
+            next = np.argmax(numerator)
+        else:
+            denominator = np.sum(numerator)
+            probabilities = numerator / denominator
+            next = np.random.choice(range(len(probabilities)), p=probabilities)
+        return next
+
+    def _remove_node(self, node):
+        self.set_of_available_nodes.remove(node)
+
+    def _evaluate(self, paths, mode):
+        """
+        Evaluates the solutions of the ants by adding up the distances between nodes.
+        :param paths: solutions from the ants
+        :param mode: max or min
+        :return: x and y coordinates of the best path as a tuple, the best path, and the best score
+        """
+        scores = np.zeros(len(paths))
+        coordinates_i = []
+        coordinates_j = []
+        for index, path in enumerate(paths):
+            score = 0
+            coords_i = []
+            coords_j = []
+            for i in range(len(path) - 1):
+                coords_i.append(path[i])
+                coords_j.append(path[i + 1])
+                score += self.map[path[i], path[i + 1]]
+            scores[index] = score
+            coordinates_i.append(coords_i)
+            coordinates_j.append(coords_j)
+        if mode == 'min':
+            best = np.argmin(scores)
+        elif mode == 'max':
+            best = np.argmax(scores)
+        return (coordinates_i[best], coordinates_j[best]), paths[best], scores[best]
+
+    def _evaporation(self):
+        """
+        Evaporate some pheromone as the inverse of the evaporation rate.  Also evaporates beta if desired.
+        """
+        self.pheromone_matrix *= (1 - self.evaporation_rate)
+        self.heuristic_beta *= (1 - self.beta_evaporation_rate)
+
+    def _intensify(self, best_coords):
+        """
+        Increases the pheromone by some scalar for the best route.
+        :param best_coords: x and y (i and j) coordinates of the best route
+        """
+        i = best_coords[0]
+        j = best_coords[1]
+        self.pheromone_matrix[i, j] += self.pheromone_intensification
+
+    def fit(self, map, iter=100, mode='min', verbose=True):  # TODO: Add verbosity
+        """
+        Fits the ACO to a specific map.  This was designed with the Traveling Salesman problem in mind.
+        :param map: Distance matrix or some other matrix with similar properties
+        :param iter: number of iterations
+        :param mode: whether to get the minimum path or maximum path
+        :return: the best score
+        """
+        if verbose: print("Beginning ACO Optimization with {} iterations...".format(iter))
+        self.map = map
+        start = time.time()
+        self._initialize()
+        for i in range(iter):
+            start_iter = time.time()
+            paths = []
+            path = []
+
+            for ant in range(self.ants):
+                current_node = self.set_of_available_nodes[np.random.randint(0, len(self.set_of_available_nodes))]
+                start_node = current_node
+                while True:
+                    path.append(current_node)
+                    self._remove_node(current_node)
+                    if len(self.set_of_available_nodes) != 0:
+                        current_node_index = self._choose_next_node(current_node)
+                        current_node = self.set_of_available_nodes[current_node_index]
+                    else:
+                        break
+
+                path.append(start_node)  # go back to start
+                self._reinstate_nodes()
+                paths.append(path)
+                path = []
+
+            best_path_coords, best_path, best_score = self._evaluate(paths, mode)
+
+            if i == 0:
+                best_score_so_far = best_score
+                self.plot_y_coord = best_score
+            else:
+                if mode == 'min':
+                    if best_score < best_score_so_far:
+                        best_score_so_far = best_score
+                        self.best_path = best_path
+                elif mode == 'max':
+                    if best_score > best_score_so_far:
+                        best_score_so_far = best_score
+                        self.best_path = best_path
+
+            self.best_series.append(best_score)
+            self._evaporation()
+            self._intensify(best_path_coords)
+            self._update_probabilities()
+
+            if verbose: print("Best score at iteration {}: {}; overall: {} ({}s)"
+                              "".format(i, round(best_score, 2), round(best_score_so_far, 2),
+                                        round(time.time() - start_iter)))
+
+        self.fit_time = round(time.time() - start)
+        self.fitted = True
+        self.plot_x_coord = iter - 1
+
+        if mode == 'min':
+            self.best = self.best_series[np.argmin(self.best_series)]
+            if verbose: print("ACO fitted.  Best score: {}".format(self.best))
+            return self.best
+        elif mode == 'max':
+            self.best = self.best_series[np.argmax(self.best_series)]
+            if verbose: print(
+                "ACO fitted.  Runtime: {} minutes.  Best score: {}".format(self.fit_time // 60, self.best))
+            return self.best
+        else:
+            raise ValueError("Invalid mode!  Choose 'min' or 'max'.")
+
+    def plot(self):
+        if not self.fitted:
+            print("Ant Colony Optimizer not fitted!  There exists nothing to plot.")
+            return None
+        else:
+            fig, ax = plt.subplots(figsize=(20, 15))
+            ax.plot(optimizer.best_series, label="Best Run")
+            ax.set_xlabel("Iteration")
+            ax.set_ylabel("Performance")
+            ax.text(.8, .8,
+                    'Ants: {}\nEvap Rate: {}\nIntensify: {}\nAlpha: {}\nBeta: {}\nBeta Evap: {}\nChoose Best: {}\n\nFit Time: {}m'.format(
+                        self.ants, self.evaporation_rate, self.pheromone_intensification, self.heuristic_alpha,
+                        self.heuristic_beta, self.beta_evaporation_rate, self.choose_best, self.fit_time // 60),
+                    bbox={'facecolor': 'gray', 'alpha': 0.8, 'pad': 10}, transform=ax.transAxes)
+            ax.legend()
+            plt.title("Ant Colony Optimization Results (best: {})".format(np.round(self.best, 2)))
+            plt.show()
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Ant-Colony-Optimization
 A Python implementation of the Ant Colony Optimization algorithm for generating solutions to such tasks as the Traveling Salesman Problem
+
+
+## Example Usage
+```problem = some_problem
+optimizer = AntColonyOptimizer(ants=10, evaporation_rate=.1, intensification=2, alpha=1, beta=1,
+                               beta_evaporation_rate=0, choose_best=.1)
+ 
+best = optimizer.fit(problem, 100)
+optimizer.plot()```
+
+## Example Plot
+
+
+###### Now 20x faster than the original!
